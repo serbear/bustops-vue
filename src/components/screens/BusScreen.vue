7305a83923bf0835ed0952291053c1d33c8cdbf6
Sergei Markov <smarkov.dev@gmail.com>
2024-01-10 12:16:18 +0200

    Bus absence message.

--- src/components/screens/BusScreen.vue
+++ src/components/screens/BusScreen.vue
@@ -11,19 +11,24 @@ const props = defineProps({
 });
 
 let isBusCardVisible = ref(false);
+let isSpinnerVisible = ref(true);
 
 watch(
   () => props.busses,
   (newVal) => {
-    isBusCardVisible = newVal !== null;
+    console.log(newVal);
+    isSpinnerVisible = newVal === null;
+
+    if (newVal !== null) {
+      isBusCardVisible = newVal.length > 0;
+    }
   },
 );
 </script>
 
 <template>
-  <div v-if="!isBusCardVisible">
+  <div v-if="isSpinnerVisible">
     <ProgreSpinner
-      v-if="!isBusCardVisible"
       :class="[elementStyles.background.screen, elementStyles.text.description]"
       class="pb-3.5 pt-7"
     />
@@ -35,11 +40,18 @@ watch(
     </p>
   </div>
   <InfoSheet
-    v-if="isBusCardVisible"
+    v-if="isBusCardVisible && !isSpinnerVisible"
     :data-array="props.busses"
     :lead-text="`Here are the buses for ${props.busStopName}:`"
     :item-key-attribute="null"
     :infoTitleName="'InfoTitleBus'"
     :element-style="elementStyles"
   />
+  <p
+    class="font-lato text-sm p-3.5 text-center"
+    :class="[elementStyles.background.screen, elementStyles.text.description]"
+    v-if="!isBusCardVisible && !isSpinnerVisible"
+  >
+    No buses were found close to the time.
+  </p>
 </template>
